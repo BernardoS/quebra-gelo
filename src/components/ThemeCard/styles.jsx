3625e907b35a627237cabb6c669b80744c3517ad
Bernardo Souza <bernardo.sfs27@gmail.com>
2024-12-30 20:51:55 -0300

    Criação da página de temas com dados mockados

---
+++ src/components/ThemeCard/styles.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+    aspect-ratio:300/90;
+    width:25%;
+    padding:8px 16px;
+    border:2px solid #1B1B1B;
+    background-color:#FFF;
+    border-radius:8px;
+    box-shadow: 10px 10px 0px 1px rgba(0,0,0,1);
+    -webkit-box-shadow: 10px 10px 0px 1px rgba(0,0,0,1);
+    -moz-box-shadow: 10px 10px 0px 1px rgba(0,0,0,1);
+    transition: transform 0.2s ease, box-shadow 0.2s ease;
+    cursor:pointer;
+    &:hover{
+        transform: scale(1.05);
+    }
+`;
+
+export const CardTitle = styled.h3`
+    font-size:18px;
+    color: #1B1B1B;
+    text-transform:capitalize;
+`;
+
+export const CardSubtitle = styled.p`
+    font-size:12px;
+    color: #1B1B1B;
+    font-weight:200;
+    line-height:2;
+`;
