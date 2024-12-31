ccae1eb36060519abaacdd4257b0cd98fa9d7c46
Bernardo Souza <bernardo.sfs27@gmail.com>
2024-12-31 17:37:04 -0300

    responsividade da home

--- src/components/Footer/style.jsx
+++ src/components/Footer/style.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import FooterBlueImageBackground from "../../assets/footer-blue.png";
+import FooterBlueImageMobileBackground from "../../assets/footer-mobile-blue.png";
 import FooterYellowImageBackground from "../../assets/footer-yellow.png";
+import { device } from "../../layoutBreakpoints";
 
 export const PrimaryFooter = styled.footer`
     display:flex;
@@ -18,6 +20,15 @@ export const PrimaryFooter = styled.footer`
         font-size:12px;
         color: #1B1B1B;
     }
+    @media ${device.tablet} {
+        background-image:url(${FooterBlueImageMobileBackground});
+        position: absolute;
+        bottom: 0;
+        z-index: -1;
+        height: 40%;
+        justify-content:flex-end;
+    }
+
 `;
 
 export const SecondaryFooter = styled.footer`
